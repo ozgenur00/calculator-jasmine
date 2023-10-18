7d2a0abde88d8a8e2af87701d878973c8fc78f91
Ozgenur Catal <117957198+ozgenur00@users.noreply.github.com>
2023-10-18 12:27:38 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -56,3 +56,31 @@ function updateMonthly(monthly) {
   const monthlyPay = document.getElementById("monthly-payment");
   monthlyPay.innerText = `$ ${monthly}`;
 }
+
+//----------------------------CALCULATOR TEST
+
+it('should calculate the monthly rate correctly', function () {
+  const values = {amount: 100000, years: 30, rate: 4.5};
+  expect(calculateMonthlyPayment(values)).toEqual("506.69");
+});
+
+//I don't know why I can't fixed to 507
+it("should return a result with 2 decimal places", function() {
+  const values = {amount: 100062, years: 30, rate: 4.5};
+  expect(calculateMonthlyPayment(values)).toEqual("507.00");
+});
+
+it('should calculate very low interest rates', function() {
+  const values = {amount: 100000, years: 10, rate: 0.1};
+  expect(calculateMonthlyPayment(values)).toEqual("837.54");
+});
+
+it('should calculate very high interest rates', function() {
+  const values = {amount: 100000, years: 2, rate: 18.9};
+  expect(calculateMonthlyPayment(values)).toEqual("5036.00");
+});
+
+it('should turn NaN if all 0', function() {
+  const values = {amount: 0, years: 0, rate: 0};
+  expect(calculateMonthlyPayment(values)).toEqual("NaN");
+});
